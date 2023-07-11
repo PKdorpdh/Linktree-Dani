b45de4ecee4ab5be3ce605783d06ffea004d81f3
PKdorpdh <lucasmartins134@hotmail.com>
2023-07-11 10:35:34 -0300

    Link tree terminado: initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const html = document.documentElement
+    
+    html.classList.toggle('lightmode')
+    // if (html.classList.contains('lightmode')) {
+    //     html.classList.remove('lightmode')
+    // } else {
+    //     html.classList.add('lightmode')
+    // }
+}
