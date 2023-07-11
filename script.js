84361ef5c910c8438984c8c5bf1763f2dc8cf868
PKdorpdh <lucasmartins134@hotmail.com>
2023-07-11 19:12:27 -0300

    Resolvendo bug do logo

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMode() {
     const html = document.documentElement
     const img = document.querySelector('.img-threads')
-    
+
     html.classList.toggle('lightmode')
     if (html.classList.contains('lightmode')) {
-        img.setAttribute('src', 'assets/images/threads.png')
+        img.setAttribute('src', './assets/images/threads.png')
     } else {
-        img.setAttribute('src', 'assets/images/threads-white.png')
+        img.setAttribute('src', './assets/images/threads-white.png')
     }
 }
